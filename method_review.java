5de8ffbcec622f88f43b42cad6bc93c817acf613
C. Fulton <cfulton@luc.edu>
2025-09-25 17:49:32 -0500

    adding changes to file

--- method_review.java
+++ method_review.java
@@ -3,12 +3,27 @@ public class method_review {
         //Array
         final String [] GROCERY_ITEMS = {"Nuts", "Beans", "Fish", "Olive Oil"};
         
+        //Overloading methods
+        outputItems(GROCERY_ITEMS); //Call/Involk method
+        
+    }//end of main method
+
+    //Define a method that accepts a string array and output the items in array
+    public static void outputItems(String [] foodArray){
         //Looping structure to output items
-        for(int i = 0; i < GROCERY_ITEMS.length; i++){
-            System.out.println(GROCERY_ITEMS[i]);
+        for(int i = 0; i < foodArray.length; i++){
+            System.out.println(foodArray[i]);
+
+
         }
 
+        outputItems(foodArray, 2);
     }
-
-    //Define an array that accepts a string array and output the items in array
+    //Define a method that accepts a string array and output the items in array
+    public static void outputItems(String [] food, int count){
+        //Looping structure to output items
+        for(int i = 0; i < count; i++){
+            System.out.println(food[i]);
+        }
+    }
 }
